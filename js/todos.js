901ca9aa2900726a5238fde91d3cd7dc0bb966b3
StanPeter <stanislav.petr7@gmail.com>
2020-12-21 21:32:54 +0100

    adding image

--- js/todos.js
+++ js/todos.js
@@ -1,24 +1,22 @@
-// variables
-var gray = "rgb(128, 128, 128)"
 
 // function to set events on each <li>
-function setEvent(tag){
+function setEvent(tag) {
     // add styiling
-    $(tag).click(function(){
+    $(tag).click(function () {
         $(this).toggleClass("todo-done");
     });
 
     //on click delete the <li>
-    $(tag).children("span").click(function(event) {
-        $(this).parent().fadeOut(500, function(){
+    $(tag).children("span").click(function (event) {
+        $(this).parent().fadeOut(500, function () {
             $(this).remove();
+        });
+        event.stopPropagation();
     });
-    event.stopPropagation(); 
-});
 }
 
 // function to add new Todo <li>
-function addTodo(){
+function addTodo() {
     var newTodo = $("<li id='added'></li>").html("<span ><i class='fa fa-trash'></i></span>" + $("#input").val());
     $("ul").append(newTodo);
 
@@ -30,14 +28,14 @@ function addTodo(){
 };
 
 // on "enter" add a new toDo <li>
-$("#input").keypress(function(event){
-    if(event.which === 13){
+$("#input").keypress(function (event) {
+    if (event.which === 13) { // enter
         addTodo();
     }
 });
 
 // fadeIn & fadeOut input text
-$("#plus-icon").click(function(){
+$("#plus-icon").click(function () {
     $("#input").fadeToggle();
 });
 
--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
-A simple Todo List created with jQuery, HTML5, CSS3
+A simple Todo List created using jQuery, HTML5, CSS3
 
+Instructions:
+
+1. Simply open index.html file in your browser (was working in chrome but others should be ok as well)
